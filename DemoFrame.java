44061bcb39b41f26ce6967659ff1936a9760b399
Bigchen8013 <44867083+Bigchen8013@users.noreply.github.com>
2018-11-22 10:16:22 +0800

    Update DemoFrame.java

--- DemoFrame.java
+++ DemoFrame.java
@@ -1,1 +1,22 @@
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+ 
+public class DemoFrame extends JFrame{
+		public DemoFrame(DemoPanel panel)
+		{
+			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			this.setSize(300, 200);
+			this.setLocationRelativeTo(null);
+			this.setTitle("LOGIN");
+			this.add(panel);
+			this.setResizable(false);
+			this.setVisible(true);
+		}
+		
+		public static void main(String[] args)
+		{
+			DemoPanel panel = new DemoPanel();
+			DemoFrame Frame = new DemoFrame(panel);
+		}
+}  
